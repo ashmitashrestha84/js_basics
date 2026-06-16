6168f0be87c77e6755f780d240d8abd860a45bd1
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-06-16 16:18:56 +0545

    add: js variables

---
+++ 1.variable.js
@@ -0,0 +1,28 @@
+//* js is dynamic, intrepretated and single threaded language
+//* reassign=var let, redeclare= var , initialize= const
+
+console.log("Hello World!");
+
+var a = 10;
+console.log(a);
+a = 15;
+console.log(a);
+a = "Ashmita";
+console.log(a);
+var a = 20;
+console.log(a);
+
+let b = 20;
+console.log(b);
+b = 10;
+console.log(b);
+b = "ashmita";
+console.log(b);
+b="";
+b=true;
+
+const c = 30;
+console.log(c);
+let d;
+var e;
+const f="";
